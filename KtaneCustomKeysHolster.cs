259f920382c4f6209db4b2828bf1e16dedd6be63
TheDarkSid3r <sid3rdark@gmail.com>
2022-11-12 18:42:56 -0800

    add the things?

---
+++ KtaneCustomKeysHolster.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RT.Json;
+using RT.Serialization;
+
+namespace KtaneCustomKeys
+{
+    public static class KtaneCustomKeysHolster
+    {
+        private static readonly Dictionary<string, Hold> s_storage = new Dictionary<string, Hold>();
+
+        private static readonly Random s_random = new Random();
+
+        private static readonly char[] s_tokenChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890".ToCharArray();
+
+        private static string CreateCode()
+        {
+            lock (s_storage)
+            {
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < 5; i++)
+                    builder.Append(s_random.Next(10));
+
+                string code = builder.ToString();
+                if (s_storage.ContainsKey(code))
+                    return CreateCode();
+
+                return code;
+            }
+        }
+
+        private static string CreateToken()
+        {
+            lock (s_storage)
+            {
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < 15; i++)
+                    builder.Append(s_tokenChars[s_random.Next(s_tokenChars.Length)]);
+
+                return builder.ToString();
+            }
+        }
+
+        public static JsonValue Push(string data)
+        {
+            lock (s_storage)
+            {
+                Hold hold = new Hold(data);
+                s_storage.Add(hold.Code, hold);
+                return ClassifyJson.Serialize(new Dictionary<string, string>
+                {
+                    { "code", hold.Code },
+                    { "token", hold.Token }
+                });
+            }
+        }
+
+        public static bool Has(string code)
+        {
+            lock (s_storage)
+                return s_storage.ContainsKey(code);
+        }
+
+        public static string Pull(string code)
+        {
+            lock (s_storage)
+                return s_storage[code].Data;
+        }
+
+        public static void Remove(string code)
+        {
+            lock (s_storage)
+                if (s_storage.ContainsKey(code))
+                    s_storage.Remove(code);
+        }
+
+        public static bool IsAuthorized(string code, string token)
+        {
+            lock (s_storage)
+                return s_storage.ContainsKey(code) && s_storage[code].Token == token;
+        }
+
+        private class Hold
+        {
+            public string Code { get; }
+
+            public string Data { get; }
+
+            public string Token { get; }
+
+            public Hold(string data)
+            {
+                Code = CreateCode();
+                Data = data;
+                Token = CreateToken();
+            }
+        }
+    }
+}
---
+++ KtaneCustomKeysPropeller.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using RT.PropellerApi;
+using RT.Servers;
+
+namespace KtaneCustomKeys
+{
+    public sealed class KtaneCustomKeysPropeller : PropellerModuleBase<KtaneCustomKeysPropellerSettings>
+    {
+        public override string Name => "KTaNE Custom Keys Propeller";
+
+        public override HttpResponse Handle(HttpRequest req)
+        {
+            switch (req.Method)
+            {
+                case HttpMethod.Post:
+                    {
+                        if (!req.Post.ContainsKey("data") || req.Post["data"].Count < 1)
+                            return HttpResponse.PlainText("Missing data.", HttpStatusCode._400_BadRequest);
+
+                        return HttpResponse.Json(KtaneCustomKeysHolster.Push(req.Post["data"].Value));
+                    }
+                case HttpMethod.Get:
+                    {
+                        string[] codes = req.Url.QueryValues("code").ToArray();
+                        if (codes.Length < 1)
+                            return HttpResponse.PlainText("Missing code.", HttpStatusCode._400_BadRequest);
+
+                        string code = codes.First();
+                        if (!KtaneCustomKeysHolster.Has(code))
+                            return HttpResponse.PlainText("Invalid code.", HttpStatusCode._404_NotFound);
+
+                        return HttpResponse.PlainText(KtaneCustomKeysHolster.Pull(code));
+                    }
+                case HttpMethod.Delete:
+                    {
+                        string[] codes = req.Url.QueryValues("code").ToArray();
+                        string[] tokens = req.Url.QueryValues("token").ToArray();
+                        if (codes.Length < 1 || tokens.Length < 1)
+                            return HttpResponse.PlainText("Missing code or token.", HttpStatusCode._400_BadRequest);
+
+                        string code = codes.First();
+                        if (!KtaneCustomKeysHolster.Has(code))
+                            return HttpResponse.PlainText("Invalid code.", HttpStatusCode._404_NotFound);
+
+                        if (!KtaneCustomKeysHolster.IsAuthorized(code, tokens.First()))
+                            return HttpResponse.PlainText("Invalid token.", HttpStatusCode._401_Unauthorized);
+
+                        KtaneCustomKeysHolster.Remove(code);
+
+                        return HttpResponse.Empty();
+                    }
+                default:
+                    return HttpResponse.PlainText("Invalid request method.", HttpStatusCode._405_MethodNotAllowed);
+            }
+        }
+    }
+
+    public sealed class KtaneCustomKeysPropellerSettings { }
+}
