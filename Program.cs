1cda806a58d9a3261c9e7b135745f7afbccb66b8
TheDarkSid3r <sid3rdark@gmail.com>
2022-11-12 19:04:46 -0800

    woo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using RT.PropellerApi;
+using RT.Util;
+
+namespace KtaneCustomKeys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PropellerUtil.RunStandalone(PathUtil.AppPathCombine("settings.json"), new KtaneCustomKeysPropeller());
+        }
+    }
+}
